a54678209239d11ececa664517ed3f6e7bf39ad1
mdsohail8073 <mds787500@gmail.com>
2026-02-10 13:34:13 +0530

    day5_internship

---
+++ src/day8_internship.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 10 11:26:20 2026
+
+@author: MD SOHAIL
+"""
+
+#using numpy
+import numpy as np
+a=5
+b=6
+r=np.multiply(a, b)
+print(r)
+
+#reshape
+import numpy as np
+a=np.array([1,2],[3,4])
+a=np.reshape(a,4)
+print(a)
+
+
+
